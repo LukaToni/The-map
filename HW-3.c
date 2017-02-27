14eee4254d0f3b79b87fe5471793d8cb05c04f4b
Luka <luka.toni93@gmail.com>
2017-02-27 20:35:15 +0100

    Optimisation

--- HW-3.c
+++ HW-3.c
@@ -3,22 +3,20 @@
 
 #define between(x, lower, upper) (lower <= x && x < upper) //function
 
-int main ()
-{
+int main () {
     int x ,y, i, j;
     scanf("%d %d", &x, &y); //read height and length
 
-
     int **map = malloc (x * sizeof (int *));
     
-    for (i = 0; i < x; i++)
+    for (i = 0; i < x; i++) {
         map[i] = malloc (y * sizeof (int));
+    }
 
-    for(i = 0; i < x; i++)
-    {
-        for (j = 0; j < y; j++)
+    for(i = 0; i < x; i++) {
+        for (j = 0; j < y; j++) {
                 map[i][j] = 0;
-                
+        }          
     }
 
     int number = 1;
@@ -26,39 +24,28 @@ int main ()
 
     int a, b;
 
-    while (scanf("%d %d", &a, &b) == 2)
-    {
-        if (between (a, 0, x) && between (b, 0 ,y))
-        {
+    while (scanf("%d %d", &a, &b) == 2) {
+        if (between (a, 0, x) && between (b, 0 ,y)) {
             map[a][b]= -1; //mine go to -1
         }
     }
 
- int k;
+    int k;
 
-    for (k = 0; k < x*y ; k++)
-    {
-        for(i = 0; i < x; i++)
-        {
-            for (j = 0; j < y; j++)
-            {
-
-                if (map[i][j] == number)
-                {
-                    if( between (i + 1, 0 ,x)  && map[i + 1][j] == -1 ) //up
-                    {
+    for (k = 0; k < x*y ; k++) {
+        for(i = 0; i < x; i++) {
+            for (j = 0; j < y; j++) {
+                 if (map[i][j] == number) {
+                    if( between (i + 1, 0 ,x)  && map[i + 1][j] == -1 ) {//up 
                         map[i + 1][j] = number + 1;
                     }
-                    if(  between (i - 1, 0 ,x) && map[i - 1][j] == -1 ) //down
-                    {
+                    if(  between (i - 1, 0 ,x) && map[i - 1][j] == -1 ) {//down
                         map[i - 1][j] = number + 1;
                     }
-                    if(  between (j + 1, 0 ,y) && map[i][j + 1] == -1 ) //right
-                    {
+                    if(  between (j + 1, 0 ,y) && map[i][j + 1] == -1 ) {//right
                         map[i][j + 1] = number + 1;
                     }
-                    if( between (j - 1, 0 ,y) && map[i][j - 1] == -1 ) //left
-                    {
+                    if( between (j - 1, 0 ,y) && map[i][j - 1] == -1 ) {//left
                         map[i][j - 1] = number + 1;
                     }
                 }
@@ -68,15 +55,13 @@ int main ()
    }
 
 //TEST TABLE
- /*  for(i = 0; i < x; i++)
-    {
-        for (j = 0; j < y; j++)
-        {
+/*  for(i = 0; i < x; i++) {
+        for (j = 0; j < y; j++) {
         printf("%4d",map[i][j]);
         }
     printf("\n\n");
     }
- */
+*/
 
     printf ("%d", map[x - 1][y - 1]-1);
     return 0;
